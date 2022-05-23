4aa7a774afa854daedd0bb3e085a7029fff3e455
JPedroBraganca <joaopedrobraganca800@gmail.com>
2022-05-23 20:01:11 -0300

    creating api

---
+++ app.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, Request
+from components.neural_network import make_prediction
+from components.image_preprocessing import preprocess_image
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Image(BaseModel):
+    image: str
+
+@app.post("/predict")
+def predict(image: Image):
+    
+    image = image.image
+    image = preprocess_image(image)
+    return make_prediction(image)
+   
+
+    
+
+    
+    
---
+++ components/neural_network.py
@@ -0,0 +1,34 @@
+from io import BytesIO
+import tensorflow as tf
+import tensorflow_hub as hub
+import numpy as np
+
+class_names = ['CAT', 'DOG']
+
+def load_model():
+
+    path = "./models/cats_vs_dogs_transfer_learning.h5"
+    load = tf.keras.models.load_model(path, custom_objects={"KerasLayer": hub.KerasLayer})
+    return load
+
+model = load_model()
+
+def make_prediction(image):
+
+    global model
+    
+    if model is None:
+        model = load_model()
+    
+    predicted = model.predict(image).tolist()[0]
+    predicted_label = np.argmax(predicted)
+
+    prob_cat = round(predicted[0], 2)
+    prob_dog = 1 - prob_cat
+    pred_class = class_names[predicted_label]
+    
+    data = {"prob_cat": prob_cat,
+            "prob_dog": prob_dog,
+            "predicted_class": pred_class}
+    
+    return data
